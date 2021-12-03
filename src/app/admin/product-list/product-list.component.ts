07c730b88b1606263e7934ef5863024c6fd7e057
thirtsoft <thirtsoft@gmail.com>
2021-12-03 22:05:59 +0000

    update user information

--- src/app/admin/product-list/product-list.component.ts
+++ src/app/admin/product-list/product-list.component.ts
@@ -6,8 +6,16 @@ import {
   ViewChild,
   TemplateRef,
 } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
 import { CommonServiceService } from '../../common-service.service';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { Router } from '@angular/router';
+
+import { UtilisateurService } from './../../services/utilisateur.service';
+import { UtilisateurDto } from './../../models/utilisateur';
+
 import * as $ from 'jquery';
 
 @Component({
@@ -22,14 +30,60 @@ export class ProductListComponent implements OnInit {
   name;
   id;
   key;
-  constructor(
-    private commonService: CommonServiceService,
-    private modalService: BsModalService
+
+  utilisateurDTOList: UtilisateurDto[];
+  p : number=1;
+  searchText;
+
+  currentTime: number = 0;
+  img: boolean;
+  editForm: FormGroup
+
+  constructor(public crudApi: UtilisateurService,
+              private router: Router,
+              public toastr: ToastrService,
+              private commonService: CommonServiceService,
+              private modalService: BsModalService,
+              private fb: FormBuilder,
   ) {}
 
   ngOnInit(): void {
     this.getProducts();
+
+    this.getUtilisateurDTOs();
+
+    if (this.crudApi.getUserAvatar(this.id) === null)
+      this.img = false;
+    else this.img = true;
+
+    this.editForm = this.fb.group({
+      id: [''],
+      name: [''],
+      username: [''],
+      address: [''],
+      mobile: [''],
+      email: ['']
+    } );
+
   }
+
+  public getUtilisateurDTOs(): void {
+    this.crudApi.getAllUtilisateurDtosOrderByIdDesc().subscribe(
+      (response: UtilisateurDto[]) => {
+        this.utilisateurDTOList = response;
+        console.log(this.utilisateurDTOList);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+
+  getTS() {
+    return this.currentTime;
+  }
+
 
   getProducts() {
     this.commonService.getProducts().subscribe(
@@ -43,22 +97,40 @@ export class ProductListComponent implements OnInit {
     );
   }
 
-  editModal(template: TemplateRef<any>, category) {
-    this.id = category.id;
-    this.name = category.name;
+  editModal(template: TemplateRef<any>, userDTO: UtilisateurDto) {
+/*     this.id = category.id;
+    this.name = category.name; */
     this.modalRef = this.modalService.show(template, {
       class: 'modal-sm modal-dialog-centered',
+    });
+
+    this.editForm.patchValue( {
+      id: userDTO.id,
+      name: userDTO.name,
+      username: userDTO.username,
+      address: userDTO.address,
+      mobile: userDTO.mobile,
+      email: userDTO.email
     });
   }
 
   update() {
+    this.crudApi.updateUtilisateurDto(this.editForm.value.id, this.editForm.value)
+      .subscribe((data) => {
+        this.modalRef.hide();
+       this.getUtilisateurDTOs();
+      })
+    this.modalRef.hide();
+  }
+
+ /*  update() {
     let params = {
       id: this.id,
       key: this.key,
       speciality: this.name,
     };
     this.modalRef.hide();
-  }
+  } */
 
   openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template, {
@@ -66,8 +138,8 @@ export class ProductListComponent implements OnInit {
     });
   }
 
-  deleteModal(template: TemplateRef<any>, product) {
-    this.id = product.id;
+  deleteModal(template: TemplateRef<any>, userDTO) {
+    this.id = userDTO.id;
     this.modalRef = this.modalService.show(template, {
       class: 'modal-sm modal-dialog-centered',
     });
@@ -82,6 +154,14 @@ export class ProductListComponent implements OnInit {
     this.commonService.deleteProduct(this.id).subscribe((data: any[]) => {
       this.modalRef.hide();
       this.getProducts();
+    });
+  }
+
+  deleteUtilisateurDTO() {
+    this.utilisateurDTOList = this.utilisateurDTOList.filter((a) => a.id !== this.id);
+    this.crudApi.deleteUtilisateurDto(this.id).subscribe((data) => {
+      this.modalRef.hide();
+      this.getUtilisateurDTOs();
     });
   }
 
--- src/app/admin/product-list/product-list.module.ts
+++ src/app/admin/product-list/product-list.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list.component';
 import { ProductListRoutingModule } from './product-list-routing.module';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+
 @NgModule({
   declarations: [ ProductListComponent ],
   imports: [
     CommonModule,
-    ProductListRoutingModule
+    ProductListRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxPaginationModule,
+    Ng2SearchPipeModule
   ]
 })
 export class ProductListModule { }
